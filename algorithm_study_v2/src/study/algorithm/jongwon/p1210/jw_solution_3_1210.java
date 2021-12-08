2e6b1611ad7b50da433a59c244f880596bc366b4
yjw8459 <ryu01410@gmail.com>
2021-12-08 10:36:43 +0900

    H-Index 멘붕

--- algorithm_study_v2/src/study/algorithm/jongwon/p1210/jw_solution_3_1210.java
+++ algorithm_study_v2/src/study/algorithm/jongwon/p1210/jw_solution_3_1210.java
@@ -1,7 +1,7 @@
 package study.algorithm.jongwon.p1210;
 
+
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -11,13 +11,60 @@ import java.util.stream.Collectors;
  */
 public class jw_solution_3_1210 {
 
-    public int solution(int[] citations) {
+
+    public int solution_1(int[] citations) {
         int answer = 0;
         List<Integer> list = Arrays.stream(citations).boxed().sorted(Comparator.reverseOrder()).collect(Collectors.toList());
-        list.stream().forEach(System.out::println);
-        for ( int i = 0; i < list.size(); i++ ){
 
+        for ( Integer target : list ){
+            int cnt = 0;    //이상인 값의 갯수
+            for ( int i = 0; i < list.size(); i++ ){
+                if ( list.get(i) >= target) cnt++;
+            }
+            if ( target <= cnt ) {
+                answer = cnt;
+                break;
+            }
         }
+
+        return answer;
+    }
+
+    /**
+     * ex)
+     * 논문     > 0 1 2 3 4 5 6
+     * H-Index > 7 6 5 4 3 2 1
+     * 논문이 H-Index보다 커질 떄까지 반복한다.
+     * H-Index의 최대 점수는 배열의 길이와 비례한다.(배열의 길이가 6이면 H-Index는 0~6)
+     * 원소 하나가 검사 범위에서 제외될 떄마다 H-Index의 점수는 줄어든다.
+     *
+     * 즉, 논문의 인용 값으로 오름차순 후 H-Index보다 논문의 인용 값이 커질 때까지 반복한다.
+     * 반복 시 마다
+     * H-Index : 총 논문 갯수 - 1
+     * 논문 인용 값 : 논문[i]
+     *
+     * @param citations
+     * @return
+     */
+    public int solution(int[] citations) {
+        int answer = 0;
+
+        // 0 1 3 5 6
+        Arrays.sort(citations);
+
+        int h;
+        for(int i=0; i<citations.length; i++){
+
+            // i일때 가장 큰 h값(논문 편수)
+            h = citations.length-i;
+
+            // 논문 인용횟수가 h 이상인지 확인
+            if(citations[i] >= h){
+                answer = h;
+                break;
+            }
+        }
+
         return answer;
     }
 
